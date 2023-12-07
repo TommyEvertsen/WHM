fe01e04e4f7014ded291dab163b1c7f0c4839cb8
tommy <tommyevertsen52@gmail.com>
2023-12-07 19:16:22 +0100

    tables

--- Warehouse-manager/resources/js/Components/Stativ.vue
+++ Warehouse-manager/resources/js/Components/Stativ.vue
@@ -1,23 +1,35 @@
 <script setup>
 defineProps(['stativ']);
+
+
+
 </script>
  
 <template>
     <div class="p-6 flex space-x-2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-        </svg>
         <div class="flex-1">
             <div class="flex justify-between items-center">
                 <div>
-                    <span class="text-gray-800">{{ stativ.name }}</span> <br>
-                    <span class="text-gray-800">Rows{{ stativ.rows }}</span> <br>
-                    <span class="text-gray-800">Columns{{ stativ.name }}</span> <br>
-                    <span class="text-gray-800">inch{{ stativ.measurementSize }}</span> <br> <br>
+                    <span class="text-gray-800">{{ stativ.name }}</span> 
+                    <span class="text-gray-800">"{{ stativ.measurementSize }}"</span> <br> <br>
                     <small class="ml-2 text-sm text-gray-600">Created at{{ new Date(stativ.created_at).toLocaleString() }}</small>
+                
+                    <table class="border-collapse border border-gray-300 mt-4">
+          <tbody>
+            <!-- Loop for rows -->
+            <tr v-for="row in Array(stativ.rows).fill(null).map((_, rowIndex) => rowIndex)" :key="row">
+              <!-- Loop for columns -->
+              <td v-for="column in Array(stativ.columns).fill(null).map((_, colIndex) => colIndex)" :key="column" class="border border-gray-300 p-2">
+                <!-- Your table cell content goes here -->
+                <!-- Example: -->
+                <!-- <div class="p-2 border border-gray-300">{{ `Row ${row + 1}, Col ${column + 1}` }}</div> -->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+                
                 </div>
             </div>
-            <p class="mt-4 text-lg text-gray-900">{{ stativ.name }}</p>
         </div>
     </div>
 </template>
